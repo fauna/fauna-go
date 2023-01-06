cc92c658a45a0ea0cd8294cadde92bc83faedef4
Darren Cunningham <cynicaljoy@gmail.com>
2023-01-06 10:25:35 -0500

    updated client

--- client.go
+++ client.go
@@ -6,10 +6,10 @@ import (
 	"fmt"
 	"io"
 	"math"
-	"net"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
 )
@@ -17,6 +17,7 @@ import (
 const (
 	EndpointProduction = "https://db.fauna.com/query/1"
 	EndpointPreview    = "https://db.fauna-preview.com/query/1"
+	EndpointLocal      = "http://localhost:8443/query/1"
 
 	EnvFaunaEndpoint         = "FAUNA_ENDPOINT"
 	EnvFaunaKey              = "FAUNA_KEY"
@@ -32,29 +33,31 @@ const (
 
 type ClientConfigFn func(*Client)
 
+// URL set the client URL
 func URL(url string) ClientConfigFn {
 	return func(c *Client) { c.url = url }
 }
 
+// HTTPClient set the http.Client for the fauna.Client
 func HTTPClient(client *http.Client) ClientConfigFn {
 	return func(c *Client) { c.http = client }
 }
 
+// Headers specify headers to be used on every http.Request
 func Headers(headers map[string]string) ClientConfigFn {
 	return func(c *Client) { c.headers = headers }
 }
 
+// Client type for
 type Client struct {
-	url            string
-	secret         string
-	headers        map[string]string
-	txnTimeEnabled bool
-	lastTxnTime    int64
-	forceTypeCheck bool
+	url                 string
+	secret              string
+	headers             map[string]string
+	txnTimeEnabled      bool
+	lastTxnTime         int64
+	typeCheckingEnabled bool
 
-	tcp      *http.Transport
-	http     *http.Client
-	endpoint *net.Addr
+	http *http.Client
 
 	// maxRetries?
 	// linearized?
@@ -62,6 +65,7 @@ type Client struct {
 	// traceParent?
 }
 
+// DefaultClient initialize fauna.Client with recommend settings
 func DefaultClient() (*Client, error) {
 	secret, found := os.LookupEnv(EnvFaunaKey)
 	if !found {
@@ -88,8 +92,26 @@ func DefaultClient() (*Client, error) {
 	), nil
 }
 
+// NewClient initialize a new fauna.Client with custom settings
 func NewClient(secret string, configFns ...ClientConfigFn) *Client {
-	client := &Client{secret: secret}
+	// sensible default
+	typeCheckEnabled := true
+	if typeCheckEnabledVal, found := os.LookupEnv(EnvFaunaTypeCheckEnabled); found {
+		// TRICKY: invert boolean check
+		typeCheckEnabled = !(strings.ToLower(typeCheckEnabledVal) == "false")
+	}
+
+	client := &Client{
+		secret: secret,
+		http:   http.DefaultClient,
+		url:    EndpointProduction,
+		headers: map[string]string{
+			HeaderAuthorization: fmt.Sprintf("Bearer %s", secret),
+		},
+		typeCheckingEnabled: typeCheckEnabled,
+	}
+
+	// set options to override defaults
 	for _, configFn := range configFns {
 		configFn(client)
 	}
@@ -97,71 +119,96 @@ func NewClient(secret string, configFns ...ClientConfigFn) *Client {
 	return client
 }
 
-// Query invoke `fql`
-func (c *Client) Query(fql string, args map[string]interface{}, obj any) error {
-	res, err := c.do(NewRequest(fql, args))
-	if err != nil {
-		return err
-	}
-
-	if unmarshalErr := json.Unmarshal(res.Data, &obj); unmarshalErr != nil {
-		return unmarshalErr
-	}
-
-	if res.Error != nil {
-		return fmt.Errorf("%s", res.Error.Message)
-	}
-
-	return nil
+// Query invoke fql with args and map to the provided obj
+func (c *Client) Query(fql string, args map[string]interface{}, obj any) (*Response, error) {
+	return c.query(fql, args, obj, c.typeCheckingEnabled)
 }
 
-func (c *Client) do(request *Request) (*Response, error) {
-	bytesOut, err := json.Marshal(request)
+// QueryPlain invoke `fql` without static checking enabled
+func (c *Client) QueryPlain(fql string, args map[string]interface{}, obj any) (*Response, error) {
+	return c.query(fql, args, obj, false)
+}
+
+type fqlRequest struct {
+	Query     string                 `json:"query"`
+	Arguments map[string]interface{} `json:"arguments,omitempty"`
+	TypeCheck bool                   `json:"typecheck"`
+}
+
+func (c *Client) query(fql string, args map[string]interface{}, obj any, typeChecking bool) (*Response, error) {
+	res, err := c.do(&fqlRequest{
+		Query:     fql,
+		Arguments: args,
+		TypeCheck: typeChecking,
+	})
 	if err != nil {
-		return nil, err
+		return res, err
 	}
 
-	request.Raw, err = http.NewRequest(http.MethodPost, c.url, bytes.NewReader(bytesOut))
-	if err != nil {
-		return nil, err
+	if obj != nil {
+		unmarshalErr := json.Unmarshal(res.Data, obj)
+		if unmarshalErr != nil {
+			return res, unmarshalErr
+		}
 	}
 
-	request.Raw.Header.Add(HeaderAuthorization, fmt.Sprintf("Bearer %s", c.secret))
+	return res, nil
+}
+
+func (c *Client) do(request *fqlRequest) (*Response, error) {
+	bytesOut, bytesErr := json.Marshal(request)
+	if bytesErr != nil {
+		return nil, bytesErr
+	}
+
+	req, reqErr := http.NewRequest(http.MethodPost, c.url, bytes.NewReader(bytesOut))
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	req.Header.Set(HeaderAuthorization, fmt.Sprintf("Bearer %s", c.secret))
 	for k, v := range c.headers {
-		request.Raw.Header.Add(k, v)
+		req.Header.Add(k, v)
 	}
 
 	if c.txnTimeEnabled {
 		if lastSeen := atomic.LoadInt64(&c.lastTxnTime); lastSeen != 0 {
-			request.Raw.Header.Add(HeaderLastSeenTxn, strconv.FormatInt(lastSeen, 10))
+			req.Header.Set(HeaderLastSeenTxn, strconv.FormatInt(lastSeen, 10))
 		}
 	}
 
-	if c.forceTypeCheck {
-		request.TypeCheck = true
-	}
-
-	r, reqErr := c.http.Do(request.Raw)
-	if reqErr != nil {
-		return nil, reqErr
+	r, doErr := c.http.Do(req)
+	if doErr != nil {
+		return nil, doErr
 	}
 	defer func() {
-		_ = request.Raw.Body.Close()
+		_ = req.Body.Close()
 	}()
+
+	var response Response
+	response.Raw = r
+
+	if r.StatusCode >= http.StatusBadRequest {
+		return &response, fmt.Errorf("request failed: [Status Code: %d] %s", r.StatusCode, r.Status)
+	}
 
 	bin, readErr := io.ReadAll(r.Body)
 	if readErr != nil {
 		return nil, readErr
 	}
 
-	var response Response
-	if unmarshalErr := json.Unmarshal(bin, &response); unmarshalErr != nil {
-		return nil, unmarshalErr
-	}
-	response.Raw = r
+	response.Bytes = bin
 
-	if txnTimeErr := c.storeLastTxnTime(response.Raw.Header); txnTimeErr != nil {
-		return nil, txnTimeErr
+	if unmarshalErr := json.Unmarshal(bin, &response); unmarshalErr != nil {
+		return &response, unmarshalErr
+	}
+
+	if txnTimeErr := c.storeLastTxnTime(r.Header); txnTimeErr != nil {
+		return &response, txnTimeErr
+	}
+
+	if response.Error != nil {
+		return &response, fmt.Errorf("%s", response.Error.Error())
 	}
 
 	return &response, nil
--- response.go
+++ response.go
@@ -2,20 +2,34 @@ package fauna
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 )
 
 type ResponseError struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    string `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func (r ResponseError) Error() string {
+	return fmt.Sprintf("%s: %s", r.Code, r.Message)
+}
+
+type Stats struct {
+	ReadOps    int           `json:"read_ops"`
+	WriteOps   int           `json:"write_ops"`
+	ComputeOps int           `json:"compute_ops"`
+	QueryTime  time.Duration `json:"query_time"`
 }
 
 type Response struct {
 	Raw        *http.Response
+	Bytes      []byte
 	Data       json.RawMessage `json:"data"`
 	Error      *ResponseError  `json:"error,omitempty"`
 	Summary    string          `json:"summary"`
 	StaticType string          `json:"static_type"`
 	TxnTime    time.Time       `json:"txn_time"`
+	Stats      *Stats          `json:"stats,omitempty"`
 }
