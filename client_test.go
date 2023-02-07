1a015dcc00f20b26a5ba81be27f1bd2281bb0df6
Darren Cunningham <darren.cunningham@fauna.com>
2023-02-07 06:31:00 -0500

    Add Test to validate Query Metadata (#33)

--- client_test.go
+++ client_test.go
@@ -40,7 +40,7 @@ func TestDefaultClient(t *testing.T) {
 			s := "foo"
 
 			var i int
-			res, queryErr := client.Query(fmt.Sprintf(`"%v".length`, s), nil, &i)
+			res, queryErr := client.Query(`arg0.length`, fauna.QueryArguments(fauna.QueryArg("arg0", s)), &i)
 			if queryErr != nil {
 				t.Errorf("%s", queryErr.Error())
 			}
@@ -54,6 +54,52 @@ func TestDefaultClient(t *testing.T) {
 			if n != i {
 				t.Errorf("expected [%d] got [%d]", n, i)
 			}
+
+			t.Run("response has expected stats headers", func(t *testing.T) {
+				if res.ByteReadOps() != 0 {
+					t.Errorf("expected no bytes read")
+				}
+
+				if res.ByteWriteOps() != 0 {
+					t.Errorf("expected no bytes written")
+				}
+
+				if res.ComputeOps() == 0 {
+					t.Errorf("should have some compute ops")
+				}
+
+				if res.FaunaBuild() == "" {
+					t.Errorf("expected a fauna build")
+				}
+
+				if res.QueryTime() == 0 {
+					t.Errorf("should have a query time")
+				}
+
+				if res.QueryBytesIn() == 0 {
+					t.Errorf("should have read query bytes")
+				}
+
+				if res.QueryBytesOut() == 0 {
+					t.Errorf("should have some query bytes out")
+				}
+
+				if res.ReadOps() > 0 || res.WriteOps() > 0 {
+					t.Errorf("should not have read/written any bytes")
+				}
+
+				if res.StorageBytesRead() > 0 || res.StorageBytesWrite() > 0 {
+					t.Errorf("should not have accessed storage")
+				}
+
+				if res.Traceparent() == "" {
+					t.Errorf("should have a traceparent")
+				}
+
+				if res.TxnRetries() > 0 {
+					t.Errorf("should not need to retry")
+				}
+			})
 		})
 
 		t.Run("Argument Request", func(t *testing.T) {
