7329ac25cc89eb924c3102204c48d1edb96fdda0
Lucas Pedroza <40873230+pnwpedro@users.noreply.github.com>
2023-03-08 11:23:40 +0100

    Add support for custom FQL templates (#43)

---
+++ template.go
@@ -0,0 +1,80 @@
+package fauna
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type templateCategory string
+
+const (
+	TemplateVariable templateCategory = "variable"
+	TemplateLiteral  templateCategory = "literal"
+)
+
+type TemplatePart struct {
+	Text     string
+	Category templateCategory
+}
+
+type template struct {
+	text string
+	re   *regexp.Regexp
+}
+
+func NewTemplate(text string) *template {
+	return &template{
+		text: text,
+		re:   regexp.MustCompile(`\$(?:(?P<escaped>\$)|{(?P<braced>[_a-zA-Z0-9]*)}|(?P<invalid>))`),
+	}
+}
+
+// Parse parses Text and returns a slice of template parts.
+func (t *template) Parse() ([]TemplatePart, error) {
+	escapedIndex := t.re.SubexpIndex("escaped")
+	bracedIndex := t.re.SubexpIndex("braced")
+	invalidIndex := t.re.SubexpIndex("invalid")
+
+	end := len(t.text)
+	currentPosition := 0
+
+	matches := t.re.FindAllStringSubmatch(t.text, -1)
+	matchIndexes := t.re.FindAllStringSubmatchIndex(t.text, -1)
+	parts := make([]TemplatePart, 0)
+
+	for i, m := range matches {
+		matchIndex := matchIndexes[i]
+		invalidStartPos := matchIndex[invalidIndex*2]
+		if invalidStartPos >= 0 {
+			// TODO: Improve with line/column num
+			return nil, fmt.Errorf("invalid placeholder in template: position %d", invalidStartPos)
+		}
+
+		matchStartPos := matchIndex[0]
+		matchEndPos := matchIndex[1]
+		escaped := m[escapedIndex]
+		variable := m[bracedIndex]
+
+		if currentPosition < matchStartPos {
+			parts = append(parts, TemplatePart{
+				Text:     t.text[currentPosition:matchStartPos] + escaped,
+				Category: TemplateLiteral,
+			})
+		}
+
+		if len(variable) > 0 {
+			parts = append(parts, TemplatePart{
+				Text:     variable,
+				Category: TemplateVariable,
+			})
+		}
+
+		currentPosition = matchEndPos
+	}
+
+	if currentPosition < end {
+		parts = append(parts, TemplatePart{Text: t.text[currentPosition:], Category: TemplateLiteral})
+	}
+
+	return parts, nil
+}
---
+++ template_test.go
@@ -0,0 +1,126 @@
+package fauna_test
+
+import (
+	"testing"
+
+	"github.com/fauna/fauna-go"
+)
+
+type TemplateSuccessCase struct {
+	given string
+	wants *[]fauna.TemplatePart
+}
+
+type TemplateErrorCase struct {
+	given string
+	error string
+}
+
+func TestTemplate_ParseSuccess(t *testing.T) {
+	testCases := []TemplateSuccessCase{
+		{
+			"let x = ${my_var}",
+			&[]fauna.TemplatePart{
+				{
+					"let x = ",
+					fauna.TemplateLiteral,
+				},
+				{
+					"my_var",
+					fauna.TemplateVariable,
+				},
+			},
+		},
+		{
+			"let x = ${my_var}\nlet y = ${my_var}\nx * y",
+			&[]fauna.TemplatePart{
+				{
+					"let x = ",
+					fauna.TemplateLiteral,
+				},
+				{
+					"my_var",
+					fauna.TemplateVariable,
+				},
+				{
+					"\nlet y = ",
+					fauna.TemplateLiteral,
+				},
+				{
+					"my_var",
+					fauna.TemplateVariable,
+				},
+				{
+					"\nx * y",
+					fauna.TemplateLiteral,
+				},
+			},
+		},
+		{
+			"${my_var} { .name }",
+			&[]fauna.TemplatePart{
+				{
+					"my_var",
+					fauna.TemplateVariable,
+				},
+				{
+					" { .name }",
+					fauna.TemplateLiteral,
+				},
+			},
+		},
+		{
+			"let x = '$${not_a_var}'",
+			&[]fauna.TemplatePart{
+				{
+					"let x = '$",
+					fauna.TemplateLiteral,
+				},
+				{
+					"{not_a_var}'",
+					fauna.TemplateLiteral,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		parsed, err := fauna.NewTemplate(tc.given).Parse()
+
+		if err != nil {
+			t.Fatalf("could not parse template: %s. err: %s", tc.given, err)
+		}
+
+		if len(parsed) != len(*tc.wants) {
+			t.Errorf("wants %q but got %q", tc.wants, parsed)
+		}
+
+		for i, tp := range parsed {
+			expected := (*tc.wants)[i]
+			if tp.Text != expected.Text && tp.Category != expected.Category {
+				t.Errorf("wants %q but got %q", expected, tp)
+			}
+		}
+	}
+}
+
+func TestTemplate_ParseFail(t *testing.T) {
+	testCases := []TemplateErrorCase{
+		{
+			"let x = ${かわいい}",
+			"invalid placeholder in template: position 9",
+		},
+	}
+
+	for _, tc := range testCases {
+		parsed, err := fauna.NewTemplate(tc.given).Parse()
+
+		if err == nil {
+			t.Fatalf("expected error, but instead we parsed %s into %q", tc.given, parsed)
+		}
+
+		if err.Error() != tc.error {
+			t.Errorf("Expected error `%s` but received `%s`", tc.error, err.Error())
+		}
+	}
+}
