bb29494d5ac6eb8f1cf975fe02ac77a09c8010d0
Cleve Stuart <90649124+cleve-fauna@users.noreply.github.com>
2023-05-10 15:09:49 -0700

    Notify @driver-release-watchers when new versin published (#96)

--- concourse/scripts/slack-message.sh
+++ concourse/scripts/slack-message.sh
@@ -5,5 +5,5 @@ cd ./fauna-go-repository
 
 PACKAGE_VERSION=$(cat version)
 
-echo "fauna-go@${PACKAGE_VERSION} has been released" > ../slack-message/publish
+echo "fauna-go@${PACKAGE_VERSION} has been released @driver-release-watchers" > ../slack-message/publish
 
