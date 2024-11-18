070950ff3c8fa009d62458a5a8faac035d51832b
James Rodewig <james.rodewig@fauna.com>
2024-11-18 14:50:49 -0500

    [DOCS-3752] Event Feeds: Document `page_size` limit

--- config.go
+++ config.go
@@ -193,6 +193,9 @@ func EventFeedStartTimeFromTime(ts time.Time) FeedOptFn {
 }
 
 // EventFeedPageSize set the page size for the [fauna.EventFeed]
-func EventFeedPageSize(ts int) FeedOptFn {
-	return func(req *feedOptions) { req.PageSize = &ts }
+// The page size is the maximum number of events returned per page.
+// Must be in the range 1 to 16000 (inclusive).
+// Defaults to 16.
+func EventFeedPageSize(pageSize int) FeedOptFn {
+	return func(req *feedOptions) { req.PageSize = &pageSize }
 }
